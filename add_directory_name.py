d9be329ba29c49569c9f4003cb5b5d3dc9e28e72
YasinEl <yelabiead@ucsd.edu>
2023-08-08 16:18:01 -0700

    add directory name function

---
+++ add_directory_name.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+
+
+def rename_with_parent(directory):
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.xyz'):
+                parent_dir = os.path.basename(root)
+                new_name = f"{parent_dir}_{file}"
+                old_path = os.path.join(root, file)
+                new_path = os.path.join(root, new_name)
+
+                os.rename(old_path, new_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Rename .xyz files by prepending the parent directory name')
+    parser.add_argument('-directory', type=str, required=True, help='Directory to search for files recursively')
+
+    args = parser.parse_args()
+
+    rename_with_parent(args.directory)
